0537bed30d6a35747dec3f9cd7cf92db0729d684
Ana Cristina <anacris_28-94@hotmail.com>
2018-11-18 19:42:00 -0600

    Comentarios y otros

--- simulacion.cc
+++ simulacion.cc
@@ -5,60 +5,66 @@
 
 using namespace std;
 
+//Llegada de un nuevo proceso
 void simulacion::E1(){
-  Reloj = E1;
-  struct proceso p = new struct;
+  Reloj = E1;                 //Adelantamos el reloj
+  struct proceso p = new struct;    //Creamos proceso
   p.horaInicio =  Reloj;
-  if(servidorOcupado){
-    colaProcesosCPU.push(p);
+  if(servidorOcupado){          //Si está ocupado el servidor
+    colaProcesosCPU.push(p);    //se va a la cola.
   }else{
-    p.inicioCPU =  Reloj;
-    int interrupcion = generarInterrupcion();
+    p.inicioCPU =  Reloj;           //Si no está ocupado el servidor
+    int interrupcion = generarInterrupcion(); //Generamos valor aleatorio
     int x;
     int tipo;
-    if (interrupcion>=70) {
+    if (interrupcion>=50) {     // Caso en que ocurre una interrupcion
       x = generarTiempoOcurrencia();
-      tipo = (interrupcion >=94)?3 : 2;
-    }else{
-      x = queantum;
+      if(interrupcion <=70){  //Caso en que se va a E/S
+        tipo = 3;
+      }
+      else{               //Caso en que sale del sistema
+        tipo = 2;
+      }
+    }else{                      //Caso en que no ocurren interrupción
+      x = quantum;
       tipo = 1;
       colaProcesosCP.push(p);
     }
-    Reloj =  E2 + x;
-    p.tiempoCPU += Reloj -p-inicioCPU;
-    E2(tipo)
+    E2 = Reloj + x;
+    p.tiempoCPU += Reloj - p.inicioCPU;
+    E2(tipo);
   }
   E1 =Reloj + generarSiguienteArribo();
 }
 
 void simulacion::E2(int tipo) {
-  Reloj =  E2;
-  if(tipo==3){
-          if(unidadESOcupado==true){
+  Reloj =  E2;                      //Movemos reloj.
+  if(tipo==3){                       //Si es el caso E/S
+          if(unidadESOcupado==true){    //Revisamos si está ocupado
               colaProcesosES.push(p);
           }else{
-              y=generarTiempoES();
-              E3=Reloj+y;
+              y=generarTiempoES();    //Si no está ocupado genera tiempo
+              E3=Reloj+y;             //Y mueve el reloj
           }
       }
-  if(tipo==1){
-          if(servidorOcupado==true){
-              colaProcesosCPU.push(p);
-          }else{
-              x=quantum;
-              colaProcesosCPU.push(p);
-          }
-          Reloj=Reloj+x;
-     }
-  if(colaProcesosCPU.tamano()>0){
-          colaProcesosCPU.pop();
-          if(tipo==1){
-              x=quantum;
-          }else{
-              x=GeneraTiempoSevicio();
-          }
-          Reloj=Reloj+x;
-          if(tipo==1){
+  if(tipo==1){                      //Caso cuando no hay interrupción
+    if(servidorOcupado==true){    //Si está ocupado
+          colaProcesosCPU.push(p);
+    }else{                        //Si no
+          x=quantum;
+          colaProcesosCPU.push(p);    //Deberíamos meterlo?
+    }
+    Reloj=Reloj+x;                //Ajustamos el reloj
+    }
+  if(colaProcesosCPU.tamano()>0){ //Si hay procesos en cola
+    colaProcesosCPU.pop();
+    if(tipo==1){
+      x=quantum;
+    }else{
+      x=GeneraTiempoSevicio();
+    }
+    Reloj=Reloj+x;
+    if(tipo==1){
               colaProcesosCPU.push(p);
           }
         }else{
